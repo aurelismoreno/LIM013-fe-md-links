c827cf638f05cf29e71c5621b305147207497259
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-10-02 23:58:23 -0500

    primer test ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = () => {
-  // ...
+
+const mdLinks = () => {};
+
+module.exports = {
+  mdLinks,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,5 @@
-const mdLinks = require("../");
+
+const mdLinks = require('../src/index.js');
 
 describe("mdLinks", () => {
   it("should...", () => {
